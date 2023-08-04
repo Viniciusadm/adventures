f7febc2b33b8dba9ddc96e53176d82354e4031d4
viniciusam <viniciusam.22@gmail.com>
2023-08-03 21:27:36 -0300

    Modal com overflow

--- resources/js/components/Site/Modal.vue
+++ resources/js/components/Site/Modal.vue
@@ -18,7 +18,7 @@ const close = () => {
         class="absolute top-0 right-0 w-full h-full bg-gray-900 bg-opacity-50 flex justify-center items-center"
         @click="close"
     >
-        <div class="bg-white rounded-lg p-4 m-4 w-[300px]" @click.stop>
+        <div class="bg-white rounded-lg p-4 m-4 w-[300px] max-h-[80vh] overflow-y-auto" @click.stop>
             <div class="flex justify-between items-center mb-4">
                 <h3 class="text-lg font-bold">{{ title }}</h3>
                 <button
