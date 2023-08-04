6f0962e51054cb405f31fe93d12db0dbf3fd1d40
viniciusam <viniciusam.22@gmail.com>
2023-08-03 22:06:00 -0300

    Tela de lista de aventuras no admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Adventure;
 use Inertia\Response;
 
 class AdminController extends Controller
@@ -9,5 +10,14 @@ class AdminController extends Controller
     public function index(): Response
     {
         return inertia('admin/Home');
+    }
+
+    public function adventures(): Response
+    {
+        $adventures = Adventure::query()->get();
+
+        return inertia('admin/Adventures', [
+            'adventures' => $adventures
+        ]);
     }
 }
---
+++ resources/js/pages/admin/Adventures.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { Adventure } from "@/types";
+import { PropType } from "vue";
+import { router } from "@inertiajs/vue3";
+
+defineProps({
+    adventures: {
+        type: Array as PropType<Adventure[]>,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <div class="flex flex-col p-4">
+        <h1 class="text-3xl font-bold mb-4">Aventuras</h1>
+
+        <button
+            v-for="adventure in adventures"
+            :key="adventure.id"
+            class="mb-4 text-start"
+            @click="router.visit(`/admin/edit/${adventure.slug}`)"
+        >
+            {{ adventure.title }}
+        </button>
+    </div>
+</template>
