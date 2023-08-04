cecaedeb2fd1546782c2876a1ed751ddec3f5a26
viniciusam <viniciusam.22@gmail.com>
2023-08-03 22:02:15 -0300

    Layout admin criado

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import Layout from "./layouts/Layout.vue";
+import Admin from "./layouts/Admin.vue";
 
 const element = document.head.querySelector('meta[name="user"]');
 const user = element.content ? JSON.parse(element.content) : null;
@@ -11,7 +12,7 @@ createInertiaApp({
     resolve: name => {
         const pages = import.meta.glob('./pages/**/*.vue', { eager: true })
         let page = pages[`./pages/${name}.vue`]
-        page.default.layout = name === 'Login' ? undefined : Layout
+        page.default.layout = name === 'Login' ? undefined : name.toString().startsWith('admin') ? Admin : Layout
         return page
     },
     setup({ el, App, props, plugin }) {
---
+++ resources/js/layouts/Admin.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { PropType } from "vue";
+import { User } from "@/types";
+import { router } from "@inertiajs/vue3";
+
+defineProps({
+    user: Object as PropType<User>,
+});
+
+const logout = () => {
+    router.visit("/logout")
+};
+</script>
+
+<template>
+    <div class="w-full max-w-[100%] mx-auto md:max-w-6xl h-screen bg-gray-100">
+        <div class="w-full h-[8vh] flex items-center justify-between px-4 border-b border-gray-300">
+            <div class="flex items-center">
+                <button
+                    class="mr-4"
+                    @click="router.visit('/')"
+                >
+                    <i class="bi bi-house-door-fill"></i> Home
+                </button>
+
+                <button
+                    class="mr-4"
+                    @click="router.visit('/admin/adventures')"
+                >
+                    <i class="bi bi-compass"></i> Aventuras
+                </button>
+            </div>
+            <div class="flex items-center">
+                <button @click="logout" class="text-red-500">Sair</button>
+            </div>
+        </div>
+        <slot />
+    </div>
+</template>
